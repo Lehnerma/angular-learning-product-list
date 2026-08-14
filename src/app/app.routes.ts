8c070b8fd0063dd7f4419eedb74a18d14d0dfc21
Max Lehner <dev@lm-lehner.at>
2026-08-14 22:41:11 +0200

    feat: add 404 page component with routing and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './shared/components/product-list/product-list';
 import { ProductDetail } from './shared/components/product-detail/product-detail';
+import { Page404 } from './shared/components/page404/page404';
 
 export const routes: Routes = [
   {
     path: '',
     component: ProductList,
   },
+  {
+    path: '404-page-not-found',
+    component: Page404,
+  },
+
+  { path: '**', redirectTo: '/404-page-not-found' },
 ];
---
+++ src/app/shared/components/page404/page404.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-page404',
+  imports: [RouterLink],
+  templateUrl: './page404.html',
+  styleUrl: './page404.scss',
+})
+
+export class Page404 {}
